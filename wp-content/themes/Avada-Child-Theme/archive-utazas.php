2c7f83e17acd9cafff9fa50d0538e613a50bb4e8
mri-dev <pocokxtrame@gmail.com>
2018-03-26 13:10:30 +0200

    Save

--- wp-content/themes/Avada-Child-Theme/archive-utazas.php
+++ wp-content/themes/Avada-Child-Theme/archive-utazas.php
@@ -11,14 +11,59 @@
 	<div class="page-width">
 		<div class="list-wrapper">
 			<div class="travels">
-				<?php foreach ($list as $travel): ?>
+				<?php foreach ($list as $travel):
+					$destionations = $travel->showDestinations();
+					$durations = $travel->showDuration();
+					$discount = $travel->getDiscount();
+					$kiemelt = $travel->isKiemelt();
+				?>
 				<article class="travel">
 					<div class="wrapper">
+						<div class="badges">
+						<?php if ($kiemelt): ?>
+							<div class="highlighted">
+								<?php echo __('Kiemelt',TD); ?>
+							</div>
+						<?php endif; ?>
+						<?php if ($discount): ?>
+							<div class="discounted">
+								<?php echo $discount['percent'].__('% leárazás',TD); ?>
+							</div>
+						<?php endif; ?>
+						</div>
 						<div class="image">
 							<a href="<?=$travel->Url()?>"><img src="<?=$travel->Image()?>" alt="<?=$travel->Title()?>"></a>
 						</div>
-						<div class="title">
-							<h3><a href="<?=$travel->Url()?>"><?=$travel->Title()?></a></h3>
+						<div class="datas">
+							<div class="title">
+								<h3><a href="<?=$travel->Url()?>"><?=$travel->Title()?></a></h3>
+							</div>
+							<div class="price">
+								<div class="old">
+									112 000 Ft
+								</div>
+								<div class="current">
+									56 000 Ft
+								</div>
+							</div>
+							<?php
+								$destionations = $travel->showDestinations();
+							?>
+							<div class="position">
+								<i class="fa fa-map-pin"></i>
+								<?php if (count($destionations) > 5): ?>
+									<span title="<?php echo implode(', ', $destionations); ?>"><?php echo sprintf(__('%d úti célt érint', TD), count($destionations)); ?></span>
+								<?php else: ?>
+									<?php echo implode(', ', $destionations); ?>
+								<?php endif; ?>
+							</div>
+							<div class="duration">
+								<i class="far fa-clock"></i> <?php if (count($durations) > 5): ?>
+									<span title="<?php echo implode(', ', $durations); ?>"><?php echo sprintf(__('%d utazási hossz elérhető', TD), count($durations)); ?></span>
+								<?php else: ?>
+									<?php echo implode(', ', $durations); ?>
+								<?php endif; ?>
+							</div>
 						</div>
 					</div>
 				</article>
--- wp-content/themes/Avada-Child-Theme/includes/class/Travel.php
+++ wp-content/themes/Avada-Child-Theme/includes/class/Travel.php
@@ -34,6 +34,135 @@ class Travel
     return get_post_permalink( $this->tpost );
   }
 
+  public function isKiemelt()
+  {
+    return true;
+  }
+
+  public function getDiscount()
+  {
+    $discount = false;
+
+    $discount = array(
+      'percent' => 45,
+      'price' => 56000
+    );
+
+    return $discount;
+  }
+
+  public function showDuration()
+  {
+    $back = array();
+
+    $raw_terms = $this->getDurations();
+
+    foreach ($raw_terms as $t) {
+      $back[] = $t['name'];
+    }
+
+    return $back;
+  }
+
+
+  public function showDestinations()
+  {
+    $back = array();
+
+    $raw_terms = $this->getDestinations();
+
+    foreach ($raw_terms as $t) {
+      $back[] = $t['name'];
+    }
+
+    return $back;
+  }
+
+  public function getDurations()
+  {
+    $list = array();
+
+    $terms = wp_get_post_terms($this->id, array(
+      'taxonomy' => 'utazas_duration'
+    ));
+
+    foreach ((array)$terms as $t) {
+      $list[] = array(
+        'name' => $t->name,
+        'obj' => $t
+      );
+    }
+
+    return $list;
+  }
+
+  public function getDestinations( $include_parent = false )
+  {
+    $list = array();
+    $parents_cnt = 0;
+    $child_cnt = 0;
+    $terms = wp_get_post_terms($this->id, array(
+      'taxonomy' => 'utazas_uticel'
+    ));
+
+    foreach ((array)$terms as $t) {
+      if($t->parent == 0) $parents_cnt++;
+      if($t->parent != 0) $child_cnt++;
+    }
+
+    // Ha csak a legfelsőbb szintű úti célok vannak és nincsenek városok
+    if ( $parents_cnt != 0 && $child_cnt == 0 )
+    {
+      foreach ((array)$terms as $t) {
+        $list[] = array(
+          'name' => $t->name,
+          'obj' => $t
+        );
+      }
+    }
+    // Ha csak alsóbb szintű term és nincs szülő megjelölve
+    else if ( $parents_cnt == 0 && $child_cnt != 0 )
+    {
+      foreach ((array)$terms as $t) {
+        $t->parentrow = $this->traceTermParent($t);
+        $list[] = array(
+          'name' => $t->name,
+          'obj' => $t
+        );
+      }
+    }
+    // Ha vegyesen van szülő és gyermek, akkor  csak a gyermek listázása
+    else if ( $parents_cnt != 0 && $child_cnt != 0 ) {
+      foreach ((array)$terms as $t) {
+        if($t->parent == 0) continue;
+        $t->parentrow = $this->traceTermParent($t);
+        $list[] = array(
+          'name' => $t->name,
+          'obj' => $t
+        );
+      }
+    }
+
+    return $list;
+  }
+
+  private function traceTermParent( \WP_Term $term )
+  {
+    $list = array();
+    $parent = (int)$term->parent;
+    $current = $term;
+
+    while ( $parent !== 0 ) {
+      $parent = (int)$current->parent;
+      if ($parent !== 0) {
+        $current = get_term($parent, 'utazas_uticel');
+        $list[] = $current;
+      }
+    }
+
+    return $list;
+  }
+
   public function __destruct()
   {
     $this->arg = null;
