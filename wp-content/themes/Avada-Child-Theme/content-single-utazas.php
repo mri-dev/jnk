6ba6c164db68b7b7aba20b68b363dfc01667ef71
mri-dev <pocokxtrame@gmail.com>
2018-03-29 15:23:53 +0200

    Save

--- wp-content/themes/Avada-Child-Theme/content-single-utazas.php
+++ wp-content/themes/Avada-Child-Theme/content-single-utazas.php
@@ -2,18 +2,33 @@
 <div class="travel-base-wrapper">
   <div class="travel-header">
     <div class="page-width">
-      HEADER
+      <div class="fake-wrapper">
+        <div class="fake-inside">
+          HEADER
+        </div>
+      </div>
     </div>
   </div>
-  <div class="travel-nav">
+  <div class="travel-nav" id="fixnav">
     <div class="page-width">
-      NAV
+      <div class="fake-wrapper">
+        <div class="fake-inside">
+          <ul>
+            <li class="active"><a href="#top"><?php echo __('Adatok', TD); ?></a></li>
+            <li><a href="#description"><?php echo __('Ismertető', TD); ?></a></li>
+            <li><a href="#programs"><?php echo __('Programok', TD); ?></a></li>
+            <li><a href="#galery"><?php echo __('Képek', TD); ?></a></li>
+            <li><a href="#reviews"><?php echo __('Értékelések', TD); ?></a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
   <div class="base-content-holder">
     <div class="travel-contents">
       <div class="description">
         <a name="description"></a>
+        <h2><i class="far fa-file-alt"></i> <?php echo __('Utazás ismertetése', TD); ?></h2>
         <?php the_content(); ?>
       </div>
     </div>
@@ -22,3 +37,29 @@
     </div>
   </div>
 </div>
+<script type="text/javascript">
+  (function($){
+    $(window).scroll(function(){
+      trackFixElement();
+    });
+
+    function trackFixElement() {
+      var navbarpos = $('.travel-header').offset().top + $('.travel-header').height()+80;
+      var sc = $(document).scrollTop();
+      var fixed = ( navbarpos < (sc+60) ) ? true : false;
+
+      if (fixed) {
+        $('.travel-base-wrapper').addClass('fixedelements');
+      } else if( !fixed) {
+        $('.travel-base-wrapper').removeClass('fixedelements');
+      }
+
+      var sidebaroffset = $('.travel-header').height() + 80 + $('.travel-nav').height() + 35 + $('.sidebar-header').height() +$('.utazas-sidebar .discount').height();
+      $('.sidebar-fix-holder').css({
+        top: (sidebaroffset * -1)
+      });
+
+      console.log(sidebaroffset);
+    }
+  })(jQuery);
+</script>
--- wp-content/themes/Avada-Child-Theme/content-utazas-sidebar.php
+++ wp-content/themes/Avada-Child-Theme/content-utazas-sidebar.php
@@ -1,3 +1,34 @@
-<div class="utazas-sidebar">
-  Utazás sidebar
+<?php
+global $travel;
+
+$destionations = $travel->showDestinations();
+$durations = $travel->showDuration();
+$discount = $travel->getDiscount();
+$kiemelt = $travel->isKiemelt();
+?>
+<div class="sidebar-fix-holder">
+  <div class="utazas-sidebar">
+    <?php if ($discount): ?>
+    <div class="discount">
+      <?php echo $discount['percent'].__('% leárazás',TD); ?>
+    </div>
+    <?php endif; ?>
+    <div class="sidebar-header">
+      <div class="swrapper">
+        <div class="price">
+          <?php if ($discount): ?>
+          <span class="old">
+            <?=number_format($travel->getOriginalPrice(), 0, '', ' ')?> <?=get_valuta()?>
+          </span>
+          <?php endif; ?>
+          <span class="current"><?=number_format($travel->getPrice(), 0, '', ' ')?> <?=get_valuta()?></span>
+        </div>
+      </div>
+    </div>
+    <div class="sidebar-content">
+      <div class="swrapper">
+        tartalom
+      </div>
+    </div>
+  </div>
 </div>
