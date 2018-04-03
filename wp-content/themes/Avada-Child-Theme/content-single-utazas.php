bc81a3a71cb21f436f3547ed30315d93c43a27c0
mri-dev <pocokxtrame@gmail.com>
2018-04-03 13:51:34 +0200

    Save

--- wp-content/themes/Avada-Child-Theme/content-single-utazas.php
+++ wp-content/themes/Avada-Child-Theme/content-single-utazas.php
@@ -1,10 +1,39 @@
 <?php global $travel; ?>
+<?php
+  $durations = $travel->showDuration();
+?>
+<a name="datas"></a>
 <div class="travel-base-wrapper">
   <div class="travel-header">
     <div class="page-width">
       <div class="fake-wrapper">
         <div class="fake-inside">
-          HEADER
+          <div class="travel-main-params">
+            <div class="durrations">
+              <i class="far fa-clock"></i>
+              <?php echo implode(', ', $durations); ?>
+            </div>
+            <div class="people-restricts">
+              <i class="fas fa-users"></i>
+              3 - 24 fő
+            </div>
+            <div class="travel-from">
+              <i class="fas fa-sign-out-alt" data-fa-transform="rotate--45"></i>
+              Budapest
+            </div>
+            <div class="travel-to">
+              <i class="fas fa-sign-in-alt" data-fa-transform="rotate-45"></i>
+              Krakkó
+            </div>
+            <div class="ellatas">
+              <i class="fas fa-utensils"></i>
+              Önellátás
+            </div>
+            <div class="travel-by">
+              <i class="fas fa-car"></i>
+              Egyéni
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -14,11 +43,11 @@
       <div class="fake-wrapper">
         <div class="fake-inside">
           <ul>
-            <li class="active"><a href="#top"><?php echo __('Adatok', TD); ?></a></li>
-            <li><a href="#description"><?php echo __('Ismertető', TD); ?></a></li>
-            <li><a href="#programs"><?php echo __('Programok', TD); ?></a></li>
-            <li><a href="#galery"><?php echo __('Képek', TD); ?></a></li>
-            <li><a href="#reviews"><?php echo __('Értékelések', TD); ?></a></li>
+            <li class="active datas"><a href="javascript:void(0);" data-scrollTarget="datas"><?php echo __('Adatok', TD); ?></a></li>
+            <li class="description"><a href="javascript:void(0);" data-scrollTarget="description"><?php echo __('Ismertető', TD); ?></a></li>
+            <li class="programs"><a href="javascript:void(0);" data-scrollTarget="programs"><?php echo __('Programok', TD); ?></a></li>
+            <li class="gallery"><a href="javascript:void(0);" data-scrollTarget="gallery"><?php echo __('Képek', TD); ?></a></li>
+            <li class="reviews"><a href="javascript:void(0);" data-scrollTarget="reviews"><?php echo sprintf(__('Értékelések (%d)', TD), 0); ?></a></li>
           </ul>
         </div>
       </div>
@@ -30,6 +59,28 @@
         <a name="description"></a>
         <h2><i class="far fa-file-alt"></i> <?php echo __('Utazás ismertetése', TD); ?></h2>
         <?php the_content(); ?>
+        <a class="gotop" href="javascript:void(0);" data-scrollTarget="datas"><?php echo __('lap tetejére', TD); ?> <i class="fas fa-long-arrow-alt-up"></i></a>
+      </div>
+      <div class="programs">
+        <a name="programs"></a>
+        <h2><i class="fas fa-tasks"></i> <?php echo __('Programok', TD); ?></h2>
+        <?php the_content(); ?>
+        <a class="gotop" href="javascript:void(0);" data-scrollTarget="datas"><?php echo __('lap tetejére', TD); ?> <i class="fas fa-long-arrow-alt-up"></i></a>
+      </div>
+      <div class="gallery">
+        <a name="gallery"></a>
+        <h2><i class="far fa-images"></i> <?php echo __('Képek', TD); ?></h2>
+        <?php the_content(); ?>
+        <a class="gotop" href="javascript:void(0);" data-scrollTarget="datas"><?php echo __('lap tetejére', TD); ?> <i class="fas fa-long-arrow-alt-up"></i></a>
+      </div>
+      <div class="reviews">
+        <a name="reviews"></a>
+        <h2><i class="far fa-star"></i> <?php echo __('Értékelések', TD); ?></h2>
+        <div class="no-item">
+          <?php echo __('Még senki nem értékelte ezt az utazást.', TD); ?><br>
+          <a href="#"><?php echo __('Értékelem az utazást', TD); ?></a>
+        </div>
+        <a class="gotop" href="javascript:void(0);" data-scrollTarget="datas"><?php echo __('lap tetejére', TD); ?> <i class="fas fa-long-arrow-alt-up"></i></a>
       </div>
     </div>
     <div class="sidebar">
@@ -41,7 +92,27 @@
   (function($){
     $(window).scroll(function(){
       trackFixElement();
+      trackNavBars();
     });
+
+    function trackNavBars() {
+      var sc = $(document).scrollTop();
+      var contentGroups = ['datas', 'description', 'programs', 'gallery', 'reviews'];
+      var currentelem = 'datas';
+
+
+      $.each(contentGroups, function(i,e){
+        var topp = $('a[name='+e+']').offset().top;
+        topp = topp - $('#fixnav').height() - 65 - 40;
+
+        if( sc >= topp) {
+          currentelem = e;
+        }
+      });
+
+      $('#fixnav ul li:not(.'+currentelem+').active').removeClass('active');
+      $('#fixnav ul li.'+currentelem).addClass('active');
+    }
 
     function trackFixElement() {
       var navbarpos = $('.travel-header').offset().top + $('.travel-header').height()+80;
@@ -54,12 +125,25 @@
         $('.travel-base-wrapper').removeClass('fixedelements');
       }
 
-      var sidebaroffset = $('.travel-header').height() + 80 + $('.travel-nav').height() + 35 + $('.sidebar-header').height() +$('.utazas-sidebar .discount').height();
+      var sidebaroffset = $('.travel-header').height() + 60 + $('.travel-nav').height() + 35 + $('.sidebar-header').height() +$('.utazas-sidebar .discount').height();
       $('.sidebar-fix-holder').css({
         top: (sidebaroffset * -1)
       });
-
-      console.log(sidebaroffset);
     }
+
+    $('*[data-scrolltarget]').click(function(){
+      var target = $(this).data('scrolltarget');
+      var t = $('a[name='+target+']');
+      var ttop = t.offset();
+
+      //$('#fixnav ul li.active').removeClass('active');
+      //$('#fixnav ul li.'+target).addClass('active');
+
+      $('body, html').stop().animate({
+        // elem távolsága a top-tól - lebegő nav magassága - sticky header magasság - 25px padding
+        scrollTop: ttop.top - $('#fixnav').height() - 65 - 25
+      }, 800);
+    });
+
   })(jQuery);
 </script>
--- wp-content/themes/Avada-Child-Theme/templates/titlebars/utazas.php
+++ wp-content/themes/Avada-Child-Theme/templates/titlebars/utazas.php
@@ -1,12 +1,18 @@
 <?php
   $travel = new Travel($post);
   $title = $post->post_title;
+  $kiemelt = $travel->isKiemelt();
 ?>
 <div class="fusion-page-title-captions single-post-caption post-caption-utazas">
   <div class="titledata">
     <h1><?php echo $title; ?></h1>
     <div class="meta">
-      <div class="cats">
+      <?php if ($kiemelt): ?>
+      <div class="kiemelt">
+        <?php echo __('Kiemelt utazás',TD); ?>
+      </div>
+      <?php endif; ?>
+      <div class="positions">
         <i class="fa fa-map-pin"></i>
         <?php echo implode(", ", $travel->showDestinations()); ?>
       </div>
