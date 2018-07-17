1dd7cfea760fa233a7b02a1cd785eaaa6c4551b7
mri-dev <pocokxtrame@gmail.com>
2018-07-17 14:52:25 +0200

    Save

--- wp-content/themes/Avada-Child-Theme/templates/shortcodes/TematikusKategoriaSc/js.php
+++ wp-content/themes/Avada-Child-Theme/templates/shortcodes/TematikusKategoriaSc/js.php
@@ -7,9 +7,14 @@
     });
 
     function autoHeightCat() {
+      var pw = $(document).width();
+      var db = 4;
+      if ( pw <= 1000) {
+        db = 2;
+      }
       var tkhw = $('.tematikus-kategoria-holder.style-boxed').width();
-      $('.tematikus-kategoria-holder.style-boxed').css({
-        height: (tkhw / 5)
+      $('.tematikus-kategoria-holder.style-boxed .programs .program').css({
+        height: (tkhw / db)
       });
     }
   })(jQuery);
